0c58835eacb60f6e94672ec301466c876f68f186
deepak chourasiya <deepak1812002@gmail.com>
2022-09-21 20:28:51 +0530

    code fix by deepak

--- prime.js
+++ prime.js
@@ -1,12 +1,18 @@
+
 function prime(num){
+    let count=0;
 for(let i=1;i<=num;i++){
 if(num%i==0){
-return true;
-}
-return false
+    count++;
 }
 }
+if(count==2){
+    return true;
+}
+}
+
 let ans=prime(9)
+
 if(ans==true){
 console.log("prime")
 }else{
